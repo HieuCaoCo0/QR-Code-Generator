e822fb6e6b9847b43a2f2c490f4ae20cea0284c6
Hieu <phambahieu2006@gmail.com>
2025-11-12 13:03:56 +0700

    code

---
+++ main.py
@@ -0,0 +1,15 @@
+import qrcode
+
+url = input("Enter your URL: ").strip()
+file_name = input("Enter the your file name: ")
+file_path = "C:\\Users\\admin\\Desktop\\" + file_name + ".png"
+
+qr = qrcode.QRCode()
+qr.add_data(url)
+img = qr.make_image()
+img.save(file_path)
+
+print("QR Code was generated in your Desktop!")
+
+
+
